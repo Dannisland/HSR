84119e782ae3b815e269751fcb863eaca3983d4c
Dannisland <1281817460@qq.com>
2025-04-16 09:57:40 +0800

    2025.4.16，2卡服务器，更新

--- base/baseTrainer.py
+++ base/baseTrainer.py
@@ -228,6 +228,46 @@ def save_checkpoint_loop3(model, model2, model3, model4, other_state: dict, sav_
     if is_best:
         shutil.copyfile(filename, join(sav_path, 'AIDN.pth.tar'))
 
+def save_checkpoint_imp(model, model2, model3, model4, other_state: dict, sav_path, is_best=False, filename='AIDN_last.pth.tar'):
+    if isinstance(model, (DistributedDataParallel, DataParallel)):
+        weight = model.module.state_dict()
+    elif isinstance(model, torch.nn.Module):
+        weight = model.state_dict()
+    else:
+        raise ValueError('model must be nn.Module or nn.DataParallel!')
+
+    if isinstance(model2, (DistributedDataParallel, DataParallel)):
+        weight2 = model2.module.state_dict()
+    elif isinstance(model2, torch.nn.Module):
+        weight2 = model2.state_dict()
+    else:
+        raise ValueError('model must be nn.Module or nn.DataParallel!')
+
+    if isinstance(model3, (DistributedDataParallel, DataParallel)):
+        weight3 = model3.module.state_dict()
+    elif isinstance(model3, torch.nn.Module):
+        weight3 = model3.state_dict()
+    else:
+        raise ValueError('model must be nn.Module or nn.DataParallel!')
+
+    if isinstance(model4, (DistributedDataParallel, DataParallel)):
+        weight4 = model4.module.state_dict()
+    elif isinstance(model4, torch.nn.Module):
+        weight4 = model4.state_dict()
+    else:
+        raise ValueError('model must be nn.Module or nn.DataParallel!')
+
+    check_makedirs(sav_path)
+
+    other_state['state_dict'] = weight
+    other_state['reveal'] = weight2
+    other_state['reveal2'] = weight3
+    other_state['imp_model'] = weight4
+
+    filename = join(sav_path, filename)
+    torch.save(other_state, filename)
+    if is_best:
+        shutil.copyfile(filename, join(sav_path, 'AIDN.pth.tar'))
 
 def save_checkpoint_loop2_imp(model, model2, model3, model4, model5, other_state: dict, sav_path, is_best=False, filename='AIDN_last.pth.tar'):
     if isinstance(model, (DistributedDataParallel, DataParallel)):
